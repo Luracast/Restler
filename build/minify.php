43eabc586bad5e7cbac69e87e4f98eb723e2c4b0
Luracast <arul@luracast.com>
2012-07-09 00:12:08 +0800

    Modifies minify to package multiple files into restler.php as it used to be in Restler 2

--- build/minify.php
+++ build/minify.php
@@ -1,23 +1,48 @@
 <?php
-header('Content-Type: text/plain');
-echo 'minifying restler php files...'.PHP_EOL;
+header ( 'Content-Type: text/plain' );
+echo 'minifying restler php files...' . PHP_EOL;
 include_once 'config.php';
-minify_all_php(RESTLER_PATH, MINIFIED_PATH, INF);
+minify_all_php ( RESTLER_PATH, MINIFIED_PATH, INF );
 echo 'done';
-function minify_all_php($from_path, $to_path, $recurse_depth=0) {
-	#echo "minify_all_php($from_path, $to_path, $recurse_depth)".PHP_EOL;
-	if(!is_dir($to_path))mkdir($to_path);
-	foreach (glob($from_path.DIRECTORY_SEPARATOR.'*.php') as $filepath) {
-		$filename=pathinfo($filepath, PATHINFO_FILENAME).'.php';
-		file_put_contents($to_path.DIRECTORY_SEPARATOR.$filename, php_strip_whitespace($filepath));
-	}
-	if($recurse_depth){
-		$recurse_depth--;
-		$dirs = array_filter(glob($from_path.DIRECTORY_SEPARATOR.'*'), 'is_dir');
-		foreach ($dirs as $dir) {
-			$dirname=substr($dir,strlen($from_path));
-			#echo $dirname.PHP_EOL;
-			minify_all_php($dir,$to_path.$dirname, $recurse_depth);
-		}
-	}
+
+function minify_all_php($from_path, $to_path, $recurse_depth = 0) {
+    // cho "minify_all_php($from_path, $to_path, $recurse_depth)".PHP_EOL;
+    if (! is_dir ( $to_path )) {
+        mkdir ( $to_path );
+    }
+    $together = array (
+            'restler.php',
+            'restexception.php',
+            'docparser.php',
+            'restlerhelper.php',
+            'restlerautoloader.php',
+            'iauthenticate.php',
+            'iformat.php',
+            'irespond.php',
+            'urlencodedformat.php',
+            'jsonformat.php',
+            'defaultresponder.php' 
+    );
+    $pack = array_fill ( 0, count ( $together ), '' );
+    foreach ( glob ( $from_path . DIRECTORY_SEPARATOR . '*.php' ) as $filepath ) {
+        $filename = pathinfo ( $filepath, PATHINFO_FILENAME ) . '.php';
+        if (in_array ( $filename, $together )) {
+            //echo array_search ( $filename, $together ) . " $filename \n";
+            $pack [array_search ( $filename, $together )] = php_strip_whitespace ( $filepath );
+        } else {
+            file_put_contents ( $to_path . DIRECTORY_SEPARATOR . $filename, php_strip_whitespace ( $filepath ) );
+        }
+    }
+    $pack = implode ( ' ', $pack );
+    $pack = '<?php' . str_replace ( '<?php', '', $pack );
+    file_put_contents ( $to_path . DIRECTORY_SEPARATOR . 'restler.php', $pack );
+    if ($recurse_depth) {
+        $recurse_depth --;
+        $dirs = array_filter ( glob ( $from_path . DIRECTORY_SEPARATOR . '*' ), 'is_dir' );
+        foreach ( $dirs as $dir ) {
+            $dirname = substr ( $dir, strlen ( $from_path ) );
+            // cho $dirname.PHP_EOL;
+            minify_all_php ( $dir, $to_path . $dirname, $recurse_depth );
+        }
+    }
 }
