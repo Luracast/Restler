92aadfab21468753daa45770a6ac39d62b6a5190
Luracast <arul@luracast.com>
2012-07-11 09:00:43 +0800

    added a filter parameter to scan only matching files

--- build/standards.php
+++ build/standards.php
@@ -5,6 +5,9 @@ include_once 'config.php';
 $command = 'php '.CODE_SNIFFER_PATH.'/scripts/phpcs --standard=PSR --report=full --report-width=80'; //--tab-width=4
 $command.= ' -l ';
 $command.= RESTLER_PATH;
+if(isset($_REQUEST['filter'])){
+    $command .= DIRECTORY_SEPARATOR.$_REQUEST['filter'];
+}
 $output=array();
 $return_var = FALSE;
 echo PHP_EOL;
