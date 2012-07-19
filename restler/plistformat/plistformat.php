b70ae58164db9c351da9e4634dda82587b1ff8a6
Luracast <arul@luracast.com>
2012-07-19 21:02:45 +0800

    fixed Plist to properly support binary plist

--- restler/plistformat/plistformat.php
+++ restler/plistformat/plistformat.php
@@ -13,89 +13,99 @@
  * @link       http://luracast.com/products/restler/
  */
 class PlistFormat implements IFormat {
-	public static $binary_mode = false;
-	const MIME_BINARY = 'application/x-plist';
-	const MIME_XML = 'application/xml';
-	const EXTENSION_BINARY = 'bplist';
-	const EXTENSION_XML = 'plist';
+    public static $binary_mode = false;
+    const MIME_BINARY = 'application/x-plist';
+    const EXTENSION_BINARY = 'bplist';
+    const MIME_XML = 'application/xml';
+    const EXTENSION_XML = 'plist';
 
-	public function getMIMEMap() {
-		return array (
-				PlistFormat::EXTENSION_BINARY => PlistFormat::MIME_BINARY,
-				PlistFormat::EXTENSION_XML => PlistFormat::MIME_XML 
-		);
-	}
+    public function getMIMEMap()
+    {
+        return array (
+                self::EXTENSION_BINARY => self::MIME_BINARY,
+                self::EXTENSION_XML => self::MIME_XML 
+        );
+    }
 
-	public function getMIME() {
-		return PlistFormat::$binary_mode ? PlistFormat::MIME_BINARY : PlistFormat::MIME_XML;
-	}
+    public function getMIME()
+    {
+        return self::$binary_mode ? self::MIME_BINARY : self::MIME_XML;
+    }
 
-	public function getExtension() {
-		return PlistFormat::$binary_mode ? PlistFormat::EXTENSION_BINARY : PlistFormat::EXTENSION_XML;
-	}
+    public function getExtension()
+    {
+        return self::$binary_mode ? self::EXTENSION_BINARY : self::EXTENSION_XML;
+    }
 
-	public function setMIME($mime) {
-		PlistFormat::$binary_mode = $mime == PlistFormat::MIME_BINARY;
-	}
+    public function setMIME($mime)
+    {
+        self::$binary_mode = $mime == self::MIME_BINARY;
+    }
 
-	public function setExtension($extension) {
-		PlistFormat::$binary_mode = $extension == PlistFormat::EXTENSION_BINARY;
-	}
+    public function setExtension($extension)
+    {
+        self::$binary_mode = $extension == self::EXTENSION_BINARY;
+    }
 
-	/**
-	 * Encode the given data in plist format
-	 *
-	 * @param array $data
-	 *        	resulting data that needs to
-	 *        	be encoded in plist format
-	 * @param boolean $humanReadable
-	 *        	set to true when restler
-	 *        	is not running in production mode. Formatter has to
-	 *        	make the encoded output more human readable
-	 * @return string encoded string
-	 */
-	public function encode($data, $humanReadable = false) {
-		require_once 'CFPropertyList.php';
-		if (! PlistFormat::$binary_mode) {
-			PlistFormat::$binary_mode = ! $humanReadable;
-		} else {
-			$humanReadable = false;
-		}
-		/**
-		 *
-		 * @var CFPropertyList
-		 */
-		$plist = new CFPropertyList ();
-		$td = new CFTypeDetector ();
-		$guessedStructure = $td->toCFType ( RestlerHelper::objectToArray ( $data ) );
-		$plist->add ( $guessedStructure );
-		return $humanReadable ? $plist->toXML ( true ) : $plist->toBinary ();
-	}
+    /**
+     * Encode the given data in plist format
+     *
+     * @param array $data
+     *            resulting data that needs to
+     *            be encoded in plist format
+     * @param boolean $humanReadable
+     *            set to true when restler
+     *            is not running in production mode. Formatter has to
+     *            make the encoded output more human readable
+     * @return string encoded string
+     */
+    public function encode($data, $humanReadable = false)
+    {
+        require_once 'CFPropertyList.php';
+        if (! self::$binary_mode) {
+            self::$binary_mode = ! $humanReadable;
+        }
+        /**
+         *
+         * @var CFPropertyList
+         */
+        $plist = new CFPropertyList ();
+        $td = new CFTypeDetector ();
+        $guessedStructure = $td->toCFType ( 
+                RestlerHelper::objectToArray ( $data ) );
+        $plist->add ( $guessedStructure );
+        return self::$binary_mode ? $plist->toBinary () : 
+            $plist->toXML ( true );
+    }
 
-	/**
-	 * Decode the given data from plist format
-	 *
-	 * @param string $data
-	 *        	data sent from client to
-	 *        	the api in the given format.
-	 * @return array associative array of the parsed data
-	 */
-	public function decode($data) {
-		require_once 'CFPropertyList.php';
-		$plist = new CFPropertyList ();
-		$plist->parse ( $data );
-		return $plist->toArray ();
-	}
+    /**
+     * Decode the given data from plist format
+     *
+     * @param string $data
+     *            data sent from client to
+     *            the api in the given format.
+     * @return array associative array of the parsed data
+     */
+    public function decode($data)
+    {
+        require_once 'CFPropertyList.php';
+        $plist = new CFPropertyList ();
+        $plist->parse ( $data );
+        return $plist->toArray ();
+    }
 
-	public function __toString() {
-		return $this->getExtension ();
-	}
+    public function __toString()
+    {
+        return $this->getExtension ();
+    }
 
-	public function setCharset($charset) {
-		// TODO Auto-generated method stub
-	}
+    public function setCharset($charset)
+    {
+        // TODO Auto-generated method stub
+    }
 
-	public function getCharset() {
-		// TODO Auto-generated method stub
-	}
+    public function getCharset()
+    {
+        // TODO Auto-generated method stub
+    }
 }
