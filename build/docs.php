9715e7667cceca160576322ac2980ed51528b10b
Luracast <arul@luracast.com>
2012-08-12 12:36:57 +0800

    developer documentation using php documenter 2

---
+++ build/docs.php
@@ -0,0 +1,28 @@
+<?php
+header ( 'Content-Type: text/plain' );
+echo 'building documentation for restler php files...' . PHP_EOL;
+include_once 'config.php';
+
+//update path environment variable for graphviz (used for creating flow charts)
+putenv ( "PATH=" . getenv('PATH'). ":/opt/local/bin/" );
+
+$command = 'cd '.PROJECT_ROOT.PHP_EOL;
+$command .= 'php ' .BUILD_DIR.DIRECTORY_SEPARATOR. PHPDOC_DIR 
+            . '/bin/phpdoc.php -d ' . RESTLER_DIR;
+$command .= ' -t ' . DOCS_DIR ;
+$command .= ' --ignore amfformat/,wadlformat/,chartformat/,debugformat/,' 
+            . 'mustacheformat/,plistformat/,yamlformat/';
+$command .= ' --title="' . API_DOC_TITLE . '"';
+
+//file_put_contents('docs.sh', $command);
+
+$output = array ();
+$return_var = FALSE;
+echo PHP_EOL;
+echo $command;
+echo PHP_EOL;
+echo PHP_EOL;
+echo exec ( $command, $output, $return_var );
+//echo exec ( 'bash ./docs.sh', $output, $return_var );
+echo implode ( PHP_EOL, $output );
+echo $return_var;
