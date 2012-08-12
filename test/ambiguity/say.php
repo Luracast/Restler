50abbc012791737a6771c64dca7f573d75a9b0cd
Luracast <arul@luracast.com>
2012-08-12 12:41:18 +0800

    testing allowAmbiguity

---
+++ test/ambiguity/say.php
@@ -0,0 +1,16 @@
+<?php
+class Say {
+	/**
+	 * @allowAmbiguity
+	 */
+	function hello($to='world') {
+		trigger_error("what?");
+		trace('Nice');
+		return "Hello $to!";
+	}
+	/*
+	function something(){
+		return 'Something';
+	}
+	*/
+}
