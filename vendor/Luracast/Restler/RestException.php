2d49feba4f335658c52aee2deae922aefaffb2bd
Luracast <arul@luracast.com>
2012-08-30 00:54:14 +0800

    Added HTTP 429 Too Many Requests. It is still in draft status but already used for rate limiting by many api providers

--- vendor/Luracast/Restler/RestException.php
+++ vendor/Luracast/Restler/RestException.php
@@ -57,6 +57,7 @@ class RestException extends Exception
         415 => 'Unsupported Media Type',
         416 => 'Requested Range Not Satisfiable',
         417 => 'Expectation Failed',
+        429 => 'Too Many Requests', //still in draft but used for rate limiting
         500 => 'Internal Server Error',
         501 => 'Not Implemented',
         502 => 'Bad Gateway',
