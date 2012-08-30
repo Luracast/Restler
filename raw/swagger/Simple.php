e91519d37e346ed5e046c3cca511aad81071d412
Luracast <arul@luracast.com>
2012-08-30 16:25:58 +0800

    updated example for swagger with public and protected api

--- raw/swagger/Simple.php
+++ raw/swagger/Simple.php
@@ -1,21 +1,31 @@
 <?php
+
 class Simple
 {
+    public function pub()
+    {
+        return "im the only api when not authenticated";
+    }
+
     /**
      * Use PHPDoc description to provide meaningful text here.
      * Long Description becomes the Implementation notes :)
      *
+     * @access protected
+     *
      * @param string $name use \@param type $name description for describing
-     * parameters like this
+     *                     parameters like this
      *  /**
      *  * some comment
      *  {@*}
      *
      * @return string
      */
-    public function get($name){
+    public function get($name)
+    {
         return $this->restler->apiMethodInfo;
         return "Worm welcome to $name!";
     }
+
 
 }
