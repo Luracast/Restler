898c4c8028c918ddb9effc8bcf4814b19fe50f40
nickl- <github@jigsoft.co.za>
2012-08-30 11:43:33 +0200

    Never have to wonder what to do with exceptions again.
    Exceptions should be taken seriously, so we have a serious exception for that. No need to be caught wondering, now what woulh Cartman use for this Exception. Call it by any athor name, this is a Luracast Restler Exception.

---
+++ vendor/Luracast/Restler/Exception.php
@@ -0,0 +1,15 @@
+<?php
+namespace Luracast\Restler;
+
+/**
+ * Class that implements a standard Exception.
+ *
+ * @category   Framework
+ * @package    restler
+ * @subpackage helper
+ * @author     Nick Lombard <github@jigsoft.co.za>
+ * @copyright  2012 Luracast
+ */
+class Exception extends \Exception
+{
+}
