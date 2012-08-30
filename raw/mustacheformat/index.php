5e4f88e1b7b4e99d01f59438d5cb69a99afaa1ac
Luracast <arul@luracast.com>
2012-08-30 18:30:18 +0800

    raw examples I used to experiment with mustache format

---
+++ raw/mustacheformat/index.php
@@ -0,0 +1,17 @@
+<?php
+require_once '../../restler3/restler.php';
+require_once '../../restler3/debugformat/debugformat.php';
+#set autoloader
+#do not use spl_autoload_register with out parameter
+#it will disable the autoloading of formats
+spl_autoload_register('spl_autoload');
+$r = new Restler();
+$r->setSupportedFormats(
+    'DebugFormat', 
+    'MustacheFormat', 
+    'JsonFormat', 
+    'XmlFormat'
+);
+$r->addAPIClass('BMI');
+$r->addAPIClass('Author');
+$r->handle();
