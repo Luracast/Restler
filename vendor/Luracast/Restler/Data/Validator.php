40e11e04b37c03ee591d21f00ff9c18dde576427
Luracast <arul@luracast.com>
2012-09-05 19:11:53 +0800

    code formatting, fix for custom class initialisation, fix array with out needing fix=true

--- vendor/Luracast/Restler/Data/Validator.php
+++ vendor/Luracast/Restler/Data/Validator.php
@@ -3,68 +3,70 @@ namespace Luracast\Restler\Data;
 
 use Luracast\Restler\RestException;
 
-class Validator implements iValidate {
+class Validator implements iValidate
+{
 
     public static function validate($input, ValidationInfo $info)
     {
         /*
-         header("Content-type: text/plain");
-         var_dump($info);
-         exit;
+        header("Content-type: text/plain");
+        var_dump($info);
+        exit;
         trace ( "validating \$$info->name with "
-                . var_export( $input, true )
-                . ' for type '.$info->type );
+            . var_export( $input, true )
+            . ' for type '.$info->type );
          */
 
-        $error = isset ( $info->rules ['message'] )
+        $error = isset ($info->rules ['message'])
             ? $info->rules ['message']
-            : "invalid value was specified for '$info->name'";
+            : "invalid value specified for '$info->name'";
 
         //if a validation method is specified
-        if (!empty( $info->method )) {
+        if (!empty($info->method)) {
             $method = $info->method;
             $info->method = '';
-            $r =self::validate ( $input, $info );
-            return $info->apiClassInstance->{$method} ( $r );
+            $r = self::validate($input, $info);
+            return $info->apiClassInstance->{$method} ($r);
         }
 
         // when type is an array check if it passes for any type
-        if (is_array ( $info->type )) {
+        if (is_array($info->type)) {
             //trace("types are ".print_r($info->type, true));
             $types = $info->type;
             foreach ($types as $type) {
                 $info->type = $type;
                 try {
-                    $r =self::validate ( $input, $info );
+                    $r = self::validate($input, $info);
                     if ($r !== false) {
                         return $r;
                     }
-                } catch ( RestException $e ) {
+                } catch (RestException $e) {
                     // just continue
                 }
             }
-            throw new RestException ( 400, $error );
+            throw new RestException (400, $error);
         }
 
         //patterns are supported only for non numeric types
-        if (isset ( $info->pattern )
-                && $info->type != 'int'
-                && $info->type != 'float'
-                && $info->type != 'number') {
-            if (! preg_match ( $info->pattern, $input )) {
-                throw new RestException ( 400, $error );
+        if (isset ($info->pattern)
+            && $info->type != 'int'
+            && $info->type != 'float'
+            && $info->type != 'number'
+        ) {
+            if (!preg_match($info->pattern, $input)) {
+                throw new RestException (400, $error);
             }
         }
 
-        if (isset ( $info->choice )) {
-            if (! in_array ( $input, $info->choice )) {
-                throw new RestException ( 400, $error );
+        if (isset ($info->choice)) {
+            if (!in_array($input, $info->choice)) {
+                throw new RestException (400, $error);
             }
         }
 
         switch ($info->type) {
             case 'email' :
-                $r = filter_var ( $input, FILTER_VALIDATE_EMAIL );
+                $r = filter_var($input, FILTER_VALIDATE_EMAIL);
                 if ($r) {
                     return $r;
                 }
@@ -72,40 +74,40 @@ class Validator implements iValidate {
             case 'int' :
             case 'float' :
             case 'number' :
-                if (! is_numeric ( $input )) {
+                if (!is_numeric($input)) {
                     break;
                 }
-                $r = $info->numericValue ( $input );
-                if (isset ( $info->min ) && $r < $info->min) {
+                $r = $info->numericValue($input);
+                if (isset ($info->min) && $r < $info->min) {
                     if ($info->fix) {
                         $r = $info->min;
                     } else {
                         break;
                     }
                 }
-                if (isset ( $info->max ) && $r > $info->max) {
+                if (isset ($info->max) && $r > $info->max) {
                     if ($info->fix) {
                         $r = $info->max;
                     } else {
                         break;
                     }
                 }
-                return $info->type=='int'
-                            ? (int) $r
-                            : ($info->type=='float' ? floatval($r) : $r);
+                return $info->type == 'int'
+                    ? (int)$r
+                    : ($info->type == 'float' ? floatval($r) : $r);
 
             case 'string' :
-                $r = strlen ( $input );
-                if (isset ( $info->min ) && $r < $info->min) {
+                $r = strlen($input);
+                if (isset ($info->min) && $r < $info->min) {
                     if ($info->fix) {
-                        $input = str_pad ( $input, $info->min, $input );
+                        $input = str_pad($input, $info->min, $input);
                     } else {
                         break;
                     }
                 }
-                if (isset ( $info->max ) && $r > $info->max) {
+                if (isset ($info->max) && $r > $info->max) {
                     if ($info->fix) {
-                        $input = substr ( $input, 0, $info->max );
+                        $input = substr($input, 0, $info->max);
                     } else {
                         break;
                     }
@@ -113,29 +115,36 @@ class Validator implements iValidate {
                 return $input;
             case 'bool':
             case 'boolean':
-                if($input=='true')return true;
-                if(is_numeric($input))return $input > 0;
+                if ($input == 'true') return true;
+                if (is_numeric($input)) return $input > 0;
                 return false;
             case 'array':
-                if(is_array($input)){
+                if (is_array($input)) {
                     return $input;
                 }
-                if($info->fix){
-                    return array($input);
-                }
+                //if ($info->fix) {
+                return array($input);
+                //}
                 break;
             case 'mixed':
             case 'unknown_type':
                 return $input;
             default :
-                if(!is_array($input)){
+                if (!is_array($input)) {
                     break;
                 }
                 //do type conversion
-                if(class_exists($info->type) && is_array(class_implements($info->type))){
-                    return call_user_func("{$info->type}::__set_state", $input);
+                if (class_exists($info->type)) {
+                    $implements = class_implements($info->type);
+                    if (is_array($implements)
+                        && in_array('Luracast\\Restler\\Data\\iValueObject',
+                            $implements)
+                    )
+                        return call_user_func(
+                            "{$info->type}::__set_state", $input
+                        );
                 }
         }
-        throw new RestException ( 400, $error );
+        throw new RestException (400, $error);
     }
 }
