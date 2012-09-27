85af5062cbeac761184c4a73a5405d14589b649b
Luracast <arul@luracast.com>
2012-09-27 23:59:35 +0800

    Fixes #71 iAuthenticate in Restler 3 RC1 Fatal error: Can't inherit abstract function Luracast\Restler\iFilter::__isAllowed() (previously declared abstract in Luracast\Restler\iAuthenticate)

--- vendor/Luracast/Restler/iAuthenticate.php
+++ vendor/Luracast/Restler/iAuthenticate.php
@@ -3,6 +3,7 @@ namespace Luracast\Restler;
 
 /**
  * Interface for creating authentication classes
+ *
  * @category   Framework
  * @package    restler
  * @subpackage auth
@@ -13,11 +14,5 @@ namespace Luracast\Restler;
  */
 interface iAuthenticate extends iFilter
 {
-    /**
-     * Auth function that is called when a protected method is requested
-     *
-     * @return boolean true when authenticated, false otherwise
-     */
-    public function __isAllowed();
 }
 
