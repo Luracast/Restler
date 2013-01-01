b7184f746a748a0275d2c97fd740d722810ced21
nickl- <github@jigsoft.co.za>
2013-01-01 12:49:57 +0200

    Add include path example

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -2,6 +2,7 @@
 function trace () {} // effectively switches DebugFormat off
 
 // add include path to test classes and include restler.php for auto loader
+set_include_path(get_include_path().PATH_SEPARATOR.__DIR__);
 include dirname(__DIR__).'/vendor/restler.php';
 Luracast\Restler\AutoLoader::addPath(__DIR__.'/vendor');
 
