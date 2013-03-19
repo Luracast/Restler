b5beb77b2155632a72fed6222d7d7d9c73546b83
Joshua Eichorn <josh@bluga.net>
2013-03-19 14:18:27 -0700

    Make rewriting of HEAD requests to GET requests configurable

--- vendor/Luracast/Restler/Defaults.php
+++ vendor/Luracast/Restler/Defaults.php
@@ -72,6 +72,11 @@ class Defaults
      */
     public static $smartAutoRouting = true;
 
+    /**
+     * @var boolean rewrites HEAD requests to GET requests
+     */
+    public static $rewriteHeadToGet = true;
+
     // ==================================================================
     //
     // Versioning
