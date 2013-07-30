4f377631faff53be8e10ceab8850dfd231fad3ce
Luracast <arul@luracast.com>
2013-07-30 19:28:49 +0800

    CommentParser to use getter method in Restler

--- vendor/Luracast/Restler/CommentParser.php
+++ vendor/Luracast/Restler/CommentParser.php
@@ -315,11 +315,12 @@ class CommentParser
                 && !empty ($matches[1])
             ) {
                 $extension = $matches[1];
-                if (isset ($this->restler->_formatMap[$extension])) {
+                $formatMap =  $this->restler->getFormatMap();
+                if (isset ($formatMap[$extension])) {
                     /**
                      * @var \Luracast\Restler\Format\iFormat
                      */
-                    $format = $this->restler->_formatMap[$extension];
+                    $format = $formatMap[$extension];
                     $format = new $format();
                     $data = $format->decode($str);
                 }
