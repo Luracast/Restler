872206aaee1f2a77d720918c0c229fb6ef5e5634
Luracast <arul@luracast.com>
2013-08-05 15:04:21 +0800

    Improving RestException to handle exceptions before all events during the `setup` stage

--- vendor/Luracast/Restler/RestException.php
+++ vendor/Luracast/Restler/RestException.php
@@ -79,7 +79,11 @@ class RestException extends Exception
     public function __construct($httpStatusCode, $errorMessage = null, array $details = array(), Exception $previous = null)
     {
         $events = Util::$restler->getEvents();
-        $this->stage = $previous ? $events[count($events)-2] : end($events);
+        if(count($events)<= 1){
+            $this->stage = 'setup';
+        } else {
+            $this->stage = $previous ? $events[count($events)-2] : end($events);
+        }
         $this->details = $details;
         parent::__construct($errorMessage, $httpStatusCode, $previous);
     }
