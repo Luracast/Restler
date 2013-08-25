f1b1334d8493477d9825b1588823db859ae16188
Luracast <arul@luracast.com>
2013-08-25 22:40:58 +0800

    Removed findOne and added comment for get method

--- vendor/Luracast/Restler/Scope.php
+++ vendor/Luracast/Restler/Scope.php
@@ -41,74 +41,21 @@ class Scope
     private static $filteredInstances = array();
 
     /**
+     * Get instance of a class
+     *
      * Apply static and non-static properties for the instance of the given
-     * class name using the method information metadata annotation provided,
-     * creating new instance when the given instance is null
+     * class name using the api method metadata creating new instance when the
+     * given instance is null
      *
      * @static
      *
-     * @param string $classNameOrInstance      name or instance of the class
-     *                                         to apply properties to
-     * @param array  $metadata                 properties as key value pairs
+     * @param string $className      name of the class
      *
      * @throws RestException
      * @internal param null|object $instance new instance is crated if set to null
      *
      * @return object instance of the specified class with properties applied
      */
-    public static function findOne($classNameOrInstance, array $metadata = null)
-    {
-        if (is_object($classNameOrInstance)) {
-            $instance = $classNameOrInstance;
-            $instance->restler = self::$restler;
-            $className = get_class($instance);
-        } else {
-            $className = $classNameOrInstance = ltrim($classNameOrInstance, '\\');
-            if (isset(self::$classAliases[$classNameOrInstance])) {
-                $classNameOrInstance = self::$classAliases[$classNameOrInstance];
-            }
-            if (!class_exists($classNameOrInstance)) {
-                throw new RestException(500, "Class '$classNameOrInstance' not found");
-            }
-            if (isset(self::$instances[$classNameOrInstance])) {
-                $instance = self::$instances[$classNameOrInstance];
-            } else {
-                $instance = new $classNameOrInstance();
-                $instance->restler = self::$restler;
-                self::$instances[$classNameOrInstance] = $instance;
-            }
-        }
-        $shortName = Util::getShortName($className);
-        $properties = null;
-        if (isset($metadata['class'][$className][CommentParser::$embeddedDataName])) {
-            $properties = $metadata['class'][$className][CommentParser::$embeddedDataName];
-
-        } elseif (isset($metadata['class'][$shortName][CommentParser::$embeddedDataName])) {
-            $properties = $metadata['class'][$shortName][CommentParser::$embeddedDataName];
-        }
-        if (isset($properties)) {
-
-            $objectVars = get_object_vars($instance);
-
-            if (is_array($properties)) {
-                foreach ($properties as $property => $value) {
-                    if (property_exists($className, $property)) {
-                        //if not a static property
-                        array_key_exists($property, $objectVars)
-                            ? $instance->{$property} = $value
-                            : $instance::$$property = $value;
-                    }
-                }
-            }
-        }
-        if ($instance instanceof iUseAuthentication && self::$restler->_authVerified) {
-            $instance->__setAuthenticationStatus
-                (self::$restler->_authenticated);
-        }
-
-        return $instance;
-    }
-
     public static function get($className)
     {
         $fullName = $className = ltrim($className, '\\');
