04ed7e46f05e64bf56b3892cdbf4644dc53672ed
Luracast <arul@luracast.com>
2013-08-29 12:37:42 +0800

    Proxy to write a log of all executed methods.

--- vendor/Luracast/Restler/Proxy.php
+++ vendor/Luracast/Restler/Proxy.php
@@ -5,14 +5,31 @@ namespace Luracast\Restler;
 class Proxy
 {
     private $instance;
+    private $class;
+    private static $log = array();
 
     public function __construct($for)
     {
         $this->instance = $for;
+        $this->class = get_class($for);
+    }
+
+    public function __destruct()
+    {
+        Scope::$restler->cache->set('log',static::$log);
+    }
+
+    public function getClass(){
+        return $this->class;
+    }
+
+    public function getInstance(){
+        return $this->instance;
     }
 
     public function __call($name, $arguments)
     {
+        static::$log[]="{$this->class}->$name()";
         return call_user_func_array(
             array($this->instance, $name),
             $arguments
@@ -21,6 +38,7 @@ class Proxy
 
     public function __callStatic($name, $arguments)
     {
+        static::$log[]="{$this->class}::$name()";
         return call_user_func_array(
             get_class($this->instance) . ':' . $name,
             $arguments
