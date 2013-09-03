6d291ae5911c1a23077adaaf93bb8df07fdcec80
Luracast <arul@luracast.com>
2013-09-03 15:20:22 +0800

    Invalid exception for Invalidating given input

---
+++ vendor/Luracast/Restler/Data/Invalid.php
@@ -0,0 +1,16 @@
+<?php
+namespace Luracast\Restler\Data;
+
+use Exception;
+
+/**
+ * Invalid Exception
+ *
+ * Used by validators to throw an exception when the given data is invalid
+ *
+ * @package Luracast\Restler\Data
+ */
+class Invalid extends Exception
+{
+
+}
