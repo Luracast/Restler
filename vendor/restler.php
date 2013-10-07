9653faaf21f28152ae0d080f3dec1c57e15be08f
Luracast <arul@luracast.com>
2013-10-07 11:50:14 +0800

    Bug fixing restler.php to find the composer autoloader in the right path. Using relative path for composer autoload.php was making api requests to fail randomly on apache server

--- vendor/restler.php
+++ vendor/restler.php
@@ -1,11 +1,11 @@
 <?php
-if (is_readable(__DIR__.'/autoload.php')) {
-    //if composer autoloader is found use it
-    $loader = require_once 'autoload.php';
+if (is_readable(__DIR__ . '/autoload.php')) {
+    //if composer auto loader is found use it
+    $loader = require_once __DIR__ . '/autoload.php';
     $loader->setUseIncludePath(true);
     class_alias('Luracast\\Restler\\Restler', 'Restler');
 } else {
-    //otherwise use the restler autoloader
+    //otherwise use the restler auto loader
     require_once 'Luracast/Restler/AutoLoader.php';
     return call_user_func(function () {
         $loader = Luracast\Restler\AutoLoader::instance();
@@ -13,4 +13,3 @@ if (is_readable(__DIR__.'/autoload.php')) {
         return $loader;
     });
 }
-
