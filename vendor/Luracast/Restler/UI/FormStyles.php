ba1257908a63a0f424bc9ccfd99acea6f175e336
Luracast <arul@luracast.com>
2013-11-20 14:38:01 +0800

    Improves FormStyles to include the accept attribute

--- vendor/Luracast/Restler/UI/FormStyles.php
+++ vendor/Luracast/Restler/UI/FormStyles.php
@@ -16,7 +16,7 @@ class FormStyles
 {
     public static $html = array(
         'form' => 'form[role=form id=$id# name=$name# method=$method# action=$action# enctype=$enctype# style="padding: 10px; background-color: #eee; border:2px solid #ddd; width:600px;"]',
-        'input' => 'div[style="display: block;"]>span[style="display: inline-block; width: 280px; text-align: right;"]>label{$label#}^input[name=$name# value=$value# type=$type# required=$required# autofocus=$autofocus# placeholder=$default#]',
+        'input' => 'div[style="display: block;"]>span[style="display: inline-block; width: 280px; text-align: right;"]>label{$label#}^input[name=$name# value=$value# type=$type# required=$required# autofocus=$autofocus# placeholder=$default# accept=$accept#]',
         'textarea' => 'div[style="display: block;"]>span[style="display: inline-block; width: 280px; text-align: right;"]>label{$label#}^textarea[name=$name# value=$value# required=$required# autofocus=$autofocus# placeholder=$default# rows=3]',
         'radio' => 'div[style="display: block;"]>span[style="display: inline-block; width: 280px; text-align: right;"]>label{$label#}^span>label*options>input[name=$name# value=$value# type=radio checked=$selected# required=$required#]+{ $text#}',
         'select' => 'div[style="display: block;"]>span[style="display: inline-block; width: 280px; text-align: right;"]>label{$label#}^select[name=$name# required=$required#]>option[value]+option[value=$value# selected=$selected#]{$text#}*options',
@@ -24,7 +24,7 @@ class FormStyles
     );
     public static $bootstrap3 = array(
         'form' => 'form[role=form id=$id# name=$name# method=$method# action=$action# enctype=$enctype#]',
-        'input' => '.form-group>label{$label#}+input.form-control[name=$name# value=$value# type=$type# required=$required# autofocus=$autofocus# placeholder=$default#]',
+        'input' => '.form-group>label{$label#}+input.form-control[name=$name# value=$value# type=$type# required=$required# autofocus=$autofocus# placeholder=$default# accept=$accept#]',
         'textarea' => '.form-group>label{$label#}+textarea.form-control[name=$name# value=$value# required=$required# autofocus=$autofocus# placeholder=$default# rows=3]',
         'radio' => '.form-group>label{$label# : &nbsp;}+label.radio-inline*options>input.radio[name=$name# value=$value# type=radio checked=$selected# required=$required#]+{$text#}',
         'select' => '.form-group>label{$label#}+select.form-control[name=$name# required=$required#]>option[value]+option[value=$value# selected=$selected#]{$text#}*options',
