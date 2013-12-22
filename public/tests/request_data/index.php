6204ca8599174839cf2fec0b799fe32844f139b4
Luracast <arul@luracast.com>
2013-12-22 23:44:51 +0800

    request data test is modified to include CSV format support

--- public/tests/request_data/index.php
+++ public/tests/request_data/index.php
@@ -8,6 +8,7 @@ use Luracast\Restler\Restler;
 require_once "../../../vendor/restler.php";
 
 $r = new Restler();
+$r->setSupportedFormats('JsonFormat','CsvFormat');
 $r->addAPIClass('Data', '');
 $r->addAPIClass('Resources');
 $r->handle();
