103abd63f83baaaa3245595f1b6238616ef16dbd
Luracast <arul@luracast.com>
2014-01-16 11:03:36 +0800

    Updated AccessControl example to set the cache identifier properly for Resources caching

--- public/examples/_010_access_control/AccessControl.php
+++ public/examples/_010_access_control/AccessControl.php
@@ -1,6 +1,7 @@
 <?php
 use \Luracast\Restler\iAuthenticate;
 use \Luracast\Restler\Resources;
+use \Luracast\Restler\Defaults;
 
 class AccessControl implements iAuthenticate
 {
@@ -11,13 +12,18 @@ class AccessControl implements iAuthenticate
     {
         //hardcoded api_key=>role for brevity
         $roles = array('12345' => 'user', '67890' => 'admin');
+        $userClass = Defaults::$userIdentifierClass;
 
-        if (!isset($_GET['api_key'])
-            || !array_key_exists($_GET['api_key'], $roles)
-        ) {
+        if (isset($_GET['api_key'])) {
+            if (!array_key_exists($_GET['api_key'], $roles)) {
+                $userClass::setCacheIdentifier($_GET['api_key']);
+                return false;
+            }
+        } else {
             return false;
         }
         static::$role = $roles[$_GET['api_key']];
+        $userClass::setCacheIdentifier(static::$role);
         Resources::$accessControlFunction = 'AccessControl::verifyAccess';
         return static::$requires == static::$role || static::$role == 'admin';
     }
