9dfa5a3566c1b8dcaf7c5d96b5b93d2c07594d80
Luracast <arul@luracast.com>
2014-02-04 21:02:58 +0800

    Updated the OAuth 2 Server example to use v1.0 of the library

--- public/examples/_015_oauth2_server/Auth/db/rebuild_db.php
+++ public/examples/_015_oauth2_server/Auth/db/rebuild_db.php
@@ -1,23 +1,33 @@
 <?php
+
 // determine where the sqlite DB will go
-$dir = __DIR__ . '/oauth.sqlite';
+$dbfile = __DIR__.'/oauth.sqlite';
 
 // remove sqlite file if it exists
-if (file_exists($dir)) {
-    unlink($dir);
+if (file_exists($dbfile)) {
+    unlink($dbfile);
+}
+
+if (!is_writable(__DIR__)) {
+    // try to set permissions.
+    if (!@chmod(__DIR__, 0777)) {
+        throw new Exception("Unable to write to $dbfile");
+    }
 }
 
 // rebuild the DB
-$db = new PDO('sqlite://' . $dir);
+$db = new PDO(sprintf('sqlite:%s', $dbfile));
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->exec('CREATE TABLE oauth_clients (client_id TEXT, client_secret TEXT, redirect_uri TEXT)');
 $db->exec('CREATE TABLE oauth_access_tokens (access_token TEXT, client_id TEXT, user_id TEXT, expires TIMESTAMP, scope TEXT)');
 $db->exec('CREATE TABLE oauth_authorization_codes (authorization_code TEXT, client_id TEXT, user_id TEXT, redirect_uri TEXT, expires TIMESTAMP, scope TEXT)');
 $db->exec('CREATE TABLE oauth_refresh_tokens (refresh_token TEXT, client_id TEXT, user_id TEXT, expires TIMESTAMP, scope TEXT)');
+$db->exec('CREATE TABLE oauth_users (username TEXT, password TEXT, first_name TEXT, last_name TEXT)');
 
 // add test data
 $db->exec('INSERT INTO oauth_clients (client_id, client_secret) VALUES ("demoapp", "demopass")');
+$db->exec(sprintf('INSERT INTO oauth_users (username, password) VALUES ("demouser", "%s")', sha1("testpass")));
 
-chmod($dir, 0777);
+chmod($dbfile, 0777);
 // $db->exec('INSERT INTO oauth_access_tokens (access_token, client_id) VALUES ("testtoken", "Some Client")');
 // $db->exec('INSERT INTO oauth_authorization_codes (authorization_code, client_id) VALUES ("testcode", "Some Client")');
