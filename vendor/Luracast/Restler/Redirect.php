7664e2a07e8a7d855adcdc8c8ff98c58d8dae1e8
Luracast <arul@luracast.com>
2014-03-08 23:25:35 +0800

    Adding to Redirect class for handling redirection

---
+++ vendor/Luracast/Restler/Redirect.php
@@ -0,0 +1,50 @@
+<?php
+namespace Luracast\Restler;
+
+use Luracast\Restler\Format\JsonFormat;
+
+/**
+ * Static class for handling redirection
+ *
+ * @category   Framework
+ * @package    Restler
+ * @author     R.Arul Kumaran <arul@luracast.com>
+ * @copyright  2010 Luracast
+ * @license    http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @link       http://luracast.com/products/restler/
+ * @version    3.0.0rc5
+ */
+class Redirect
+{
+    /**
+     * Redirect to given url
+     *
+     * @param string $url relative path or full url
+     * @param array $params associative array of query parameters
+     * @param array $sessionData associative array of properties to be set in $_SESSION
+     *
+     * @return array
+     */
+    public static function to($url, array $params = array(), array $sessionData = array())
+    {
+        $url = ltrim($url, '/');
+        /** @var $r Restler */
+        $r = Scope::get('Restler');
+        $base = $r->getBaseUrl() . '/';
+        if (0 !== strpos($url, 'http'))
+            $url = $base . $url;
+        if (!empty($sessionData) || $base . $r->url !== $url || Util::getRequestMethod() != 'GET') {
+            if ($r->responseFormat instanceof JsonFormat)
+                return array('redirect' => $url);
+            if (!empty($params)) {
+                $url .= '?' . http_build_query($params);
+            }
+            foreach ($sessionData as $name => $value) {
+                $_SESSION[$name] = $value;
+            }
+            header("Location: $url");
+            die('');
+        }
+        return array();
+    }
+}
