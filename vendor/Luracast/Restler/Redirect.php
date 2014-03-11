030470643552f54f73c9cb85b8385d5f1609d366
Luracast <arul@luracast.com>
2014-03-11 23:55:58 +0800

    Improves Redirect with flash message support using the new Flash class

--- vendor/Luracast/Restler/Redirect.php
+++ vendor/Luracast/Restler/Redirect.php
@@ -19,14 +19,14 @@ class Redirect
     /**
      * Redirect to given url
      *
-     * @param string $url relative path or full url
-     * @param array $params associative array of query parameters
-     * @param array $sessionData associative array of properties to be set in $_SESSION
-     * @param int $status http status code to send the response with ideally 301 or 302
+     * @param string $url       relative path or full url
+     * @param array  $params    associative array of query parameters
+     * @param array  $flashData associative array of properties to be set in $_SESSION for one time use
+     * @param int    $status    http status code to send the response with ideally 301 or 302
      *
      * @return array
      */
-    public static function to($url, array $params = array(), array $sessionData = array(), $status = 302)
+    public static function to($url, array $params = array(), array $flashData = array(), $status = 302)
     {
         $url = ltrim($url, '/');
         /** @var $r Restler */
@@ -34,15 +34,13 @@ class Redirect
         $base = $r->getBaseUrl() . '/';
         if (0 !== strpos($url, 'http'))
             $url = $base . $url;
-        if (!empty($sessionData) || $base . $r->url !== $url || Util::getRequestMethod() != 'GET') {
+        if (!empty($flashData) || $base . $r->url !== $url || Util::getRequestMethod() != 'GET') {
             if ($r->responseFormat instanceof JsonFormat)
                 return array('redirect' => $url);
             if (!empty($params)) {
                 $url .= '?' . http_build_query($params);
             }
-            foreach ($sessionData as $name => $value) {
-                $_SESSION[$name] = $value;
-            }
+            Flash::set($flashData);
             header(
                 "{$_SERVER['SERVER_PROTOCOL']} $status " .
                 (isset(RestException::$codes[$status]) ? RestException::$codes[$status] : '')
