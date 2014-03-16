4b96ae709c7a81210531639b175768ebe4eface1
Luracast <arul@luracast.com>
2014-03-17 00:57:59 +0800

    Improves FormStyles to include Zurb Foundation 5

--- vendor/Luracast/Restler/UI/FormStyles.php
+++ vendor/Luracast/Restler/UI/FormStyles.php
@@ -21,6 +21,7 @@ class FormStyles
         'radio' => 'div[style="display: block;"]>span[style="display: inline-block; width: 280px; text-align: right;"]>label{$label#}^span>label*options>input[name=$name# value=$value# type=radio checked=$selected# required=$required#]+{ $text#}',
         'select' => 'div[style="display: block;"]>span[style="display: inline-block; width: 280px; text-align: right;"]>label{$label#}^select[name=$name# required=$required#]>option[value]+option[value=$value# selected=$selected#]{$text#}*options',
         'submit' => 'div[style="display: block;"]>span[style="display: inline-block; width: 280px;"]>label{ &nbsp; }^button.btn.btn-primary[type=submit]{$label#}',
+        'fieldset' => 'fieldset>legend{$label#}',
     );
     public static $bootstrap3 = array(
         'form' => 'form[role=form id=$id# name=$name# method=$method# action=$action# enctype=$enctype#]',
@@ -29,5 +30,15 @@ class FormStyles
         'radio' => '.form-group>label{$label# : &nbsp;}+label.radio-inline*options>input.radio[name=$name# value=$value# type=radio checked=$selected# required=$required#]+{$text#}',
         'select' => '.form-group>label{$label#}+select.form-control[name=$name# required=$required#]>option[value]+option[value=$value# selected=$selected#]{$text#}*options',
         'submit' => 'button.btn.btn-primary[type=submit]{$label#}',
+        'fieldset' => 'fieldset>legend{$label#}',
+    );
+    public static $foundation5 = array(
+        'form' => 'form.large-6.column[id=$id# name=$name# method=$method# action=$action# enctype=$enctype#]',
+        'input' => 'label{$label#}+input[name=$name# value=$value# type=$type# required=$required# autofocus=$autofocus# placeholder=$default# accept=$accept#]',
+        'textarea' => 'label{$label#}+textarea[name=$name# value=$value# required=$required# autofocus=$autofocus# placeholder=$default# rows=3]',
+        'radio' => 'label{$label# : &nbsp;}+label.radio-inline*options>input.radio[name=$name# value=$value# type=radio checked=$selected# required=$required#]+{$text#}',
+        'select' => 'label{$label#}+select[name=$name# required=$required#]>option[value]+option[value=$value# selected=$selected#]{$text#}*options',
+        'submit' => 'button.button[type=submit]{$label#}',
+        'fieldset' => 'fieldset>legend{$label#}',
     );
 }
