d0fd1d141bd981cd33a7671d19249d86a9352ef5
Luracast <arul@luracast.com>
2014-04-19 21:36:00 +0530

    Adding String utility class

---
+++ vendor/Luracast/Restler/Data/String.php
@@ -0,0 +1,35 @@
+<?php
+namespace Luracast\Restler\Data;
+
+/**
+ * Convenience class for String manipulation
+ *
+ * @category   Framework
+ * @package    Restler
+ * @author     R.Arul Kumaran <arul@luracast.com>
+ * @copyright  2010 Luracast
+ * @license    http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @link       http://luracast.com/products/restler/
+ */
+class String
+{
+    static public function beginsWith($haystack, $needle)
+    {
+        $length = strlen($needle);
+        return (substr($haystack, 0, $length) === $needle);
+    }
+
+    static public function endsWith($haystack, $needle)
+    {
+        $length = strlen($needle);
+        if ($length == 0) {
+            return true;
+        }
+        return (substr($haystack, -$length) === $needle);
+    }
+
+    public static function title($name)
+    {
+        return ucfirst(preg_replace(array('/(?<=[^A-Z])([A-Z])/', '/(?<=[^0-9])([0-9])/'), ' $0', $name));
+    }
+} 
