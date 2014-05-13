51aaaa8fd3b57a6044c87468b9c3b309b9a5bcfd
Luracast <arul@luracast.com>
2014-05-13 21:21:27 +0800

    FormStyles updated. Improved checkbox, radio and select rendering. Removed hardcoded styles in html forms

--- vendor/Luracast/Restler/UI/FormStyles.php
+++ vendor/Luracast/Restler/UI/FormStyles.php
@@ -15,22 +15,32 @@ namespace Luracast\Restler\UI;
 class FormStyles
 {
     public static $html = array(
-        'form' => 'form[role=form id=$id# name=$name# method=$method# action=$action# enctype=$enctype# style="padding: 10px; background-color: #eee; border:2px solid #ddd; width:600px;"]',
-        'input' => 'div[style="display: block;"]>span[style="display: inline-block; width: 280px; text-align: right;"]>label{$label#}^input[name=$name# value=$value# type=$type# required=$required# autofocus=$autofocus# placeholder=$default# accept=$accept#]',
-        'textarea' => 'div[style="display: block;"]>span[style="display: inline-block; width: 280px; text-align: right;"]>label{$label#}^textarea[name=$name# required=$required# autofocus=$autofocus# placeholder=$default# rows=3]{$value#}',
-        'radio' => 'div[style="display: block;"]>span[style="display: inline-block; width: 280px; text-align: right;"]>label{$label#}^span>label*options>input[name=$name# value=$value# type=radio checked=$selected# required=$required#]+{ $text#}',
-        'select' => 'div[style="display: block;"]>span[style="display: inline-block; width: 280px; text-align: right;"]>label{$label#}^select[name=$name# required=$required#]>option[value]+option[value=$value# selected=$selected#]{$text#}*options',
-        'submit' => 'div[style="display: block;"]>span[style="display: inline-block; width: 280px;"]>label{ &nbsp; }^button.btn.btn-primary[type=submit]{$label#}',
+        'form' => 'form[role=form id=$id# name=$name# method=$method# action=$action# enctype=$enctype#]',
+        'input' => '.row>section>label{$label#}^input[name=$name# value=$value# type=$type# required=$required# autofocus=$autofocus# placeholder=$default# accept=$accept#]',
+        'textarea' => '.row>label{$label#}^textarea[name=$name# required=$required# autofocus=$autofocus# placeholder=$default# rows=3]{$value#}',
+        'radio' => '.row>section>label{$label#}^span>label*options>input[name=$name# value=$value# type=radio checked=$selected# required=$required#]+{ $text#}',
+        'select' => '.row>label{$label#}^select[name=$name# required=$required#]>option[value]+option[value=$value# selected=$selected#]{$text#}*options',
+        'submit' => '.row>label{ &nbsp; }^button[type=submit]{$label#}',
         'fieldset' => 'fieldset>legend{$label#}',
+        'checkbox' => '.row>label>input[name=$name# value=$value# type=checkbox checked=$selected# required=$required# autofocus=$autofocus# accept=$accept#]+{$label#}',
+        //------------- TYPE BASED STYLES ---------------------//
+        'checkbox-array' => 'fieldset>legend{$label#}+section*options>label>input[name=$name# value=$value# type=checkbox checked=$selected# required=$required# autofocus=$autofocus# accept=$accept#]+{ $text#}',
+        'select-array' => 'label{$label#}+select[name=$name# required=$required# multiple style="height: auto;background-image: none; outline: inherit;"]>option[value=$value# selected=$selected#]{$text#}*options',
     );
     public static $bootstrap3 = array(
         'form' => 'form[role=form id=$id# name=$name# method=$method# action=$action# enctype=$enctype#]',
         'input' => '.form-group>label{$label#}+input.form-control[name=$name# value=$value# type=$type# required=$required# autofocus=$autofocus# placeholder=$default# accept=$accept#]',
         'textarea' => '.form-group>label{$label#}+textarea.form-control[name=$name# required=$required# autofocus=$autofocus# placeholder=$default# rows=3]{$value#}',
-        'radio' => '.form-group>label{$label# : &nbsp;}+label.radio-inline*options>input.radio[name=$name# value=$value# type=radio checked=$selected# required=$required#]+{$text#}',
-        'select' => '.form-group>label{$label#}+select.form-control[name=$name# required=$required#]>option[value]+option[value=$value# selected=$selected#]{$text#}*options',
+        'radio' => 'fieldset>legend{$label#}>.radio*options>label>input.radio[name=$name# value=$value# type=radio checked=$selected# required=$required#]{$text#}',
+        'select' => '.form-group>label{$label#}+select.form-control[name=$name# multiple=$multiple# required=$required#]>option[value]+option[value=$value# selected=$selected#]{$text#}*options',
         'submit' => 'button.btn.btn-primary[type=submit]{$label#}',
         'fieldset' => 'fieldset>legend{$label#}',
+        'checkbox' => '.checkbox>label>input[name=$name# value=$value# type=checkbox checked=$selected# required=$required# autofocus=$autofocus# accept=$accept#]+{$label#}',
+        //------------- TYPE BASED STYLES ---------------------//
+        'checkbox-array' => 'fieldset>legend{$label#}>.checkbox*options>label>input[name=$name# value=$value# type=checkbox checked=$selected# required=$required#]{$text#}',
+        'select-array' => '.form-group>label{$label#}+select.form-control[name=$name# multiple=$multiple# required=$required#] size=$options#>option[value=$value# selected=$selected#]{$text#}*options',
+        //------------- CUSTOM STYLES ---------------------//
+        'radio-inline' => '.form-group>label{$label# : &nbsp;}+label.radio-inline*options>input.radio[name=$name# value=$value# type=radio checked=$selected# required=$required#]+{$text#}',
     );
     public static $foundation5 = array(
         'form' => 'form[id=$id# name=$name# method=$method# action=$action# enctype=$enctype#]',
@@ -40,5 +50,10 @@ class FormStyles
         'select' => 'label{$label#}+select[name=$name# required=$required#]>option[value]+option[value=$value# selected=$selected#]{$text#}*options',
         'submit' => 'button.button[type=submit]{$label#}',
         'fieldset' => 'fieldset>legend{$label#}',
+        'checkbox' => 'label>input[name=$name# value=$value# type=checkbox checked=$selected# required=$required# autofocus=$autofocus# accept=$accept#]+{ $label#}',
+        //------------- TYPE BASED STYLES ---------------------//
+        'checkbox-array' => 'fieldset>legend{$label#}+label*options>input[name=$name# value=$value# type=checkbox checked=$selected# required=$required# autofocus=$autofocus# accept=$accept#]+{ $text#}',
+        'select-array' => 'label{$label#}+select[name=$name# required=$required# multiple style="height: auto;background-image: none; outline: inherit;"]>option[value=$value# selected=$selected#]{$text#}*options',
+        //------------- CUSTOM STYLES ---------------------//
     );
 }
