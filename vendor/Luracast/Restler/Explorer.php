99988da734545a6d4cacb197e5b13106fde106da
Luracast <arul@luracast.com>
2014-06-06 23:37:37 +0800

    First functional version of Explorer. For usage example look at `public/tests/param` folder

--- vendor/Luracast/Restler/Explorer.php
+++ vendor/Luracast/Restler/Explorer.php
@@ -3,6 +3,7 @@ namespace Luracast\Restler;
 
 use stdClass;
 use Luracast\Restler\Data\String;
+use Luracast\Restler\Data\ValidationInfo;
 
 class Explorer
 {
@@ -62,8 +63,8 @@ class Explorer
      * used to set the extension manually
      */
     public $formatString = '';
-    protected $_models;
-    protected $_bodyParam;
+    protected $models = array();
+    protected $bodyParam;
     /**
      * @var bool|stdClass
      */
@@ -85,6 +86,27 @@ class Explorer
     );
     protected $_authenticated = false;
     protected $cacheName = '';
+
+    /**
+     * @var array type mapping for converting data types to JSON-Schema Draft 4
+     * Which is followed by swagger 1.2 spec
+     */
+    public static $dataTypeAlias = array(
+        //'string' => 'string',
+        'int' => 'integer',
+        'number' => 'number',
+        'float' => array('number', 'float'),
+        'bool' => 'boolean',
+        //'boolean' => 'boolean',
+        //'NULL' => 'null',
+        'array' => 'array',
+        //'object' => 'object',
+        'stdClass' => 'object',
+        'mixed' => 'string',
+        'date' => array('string', 'date'),
+        'datetime' => array('string', 'date-time'),
+    );
+
     /**
      * @var string base path for explorer
      */
@@ -104,7 +126,7 @@ class Explorer
      */
     public function get()
     {
-        if (func_get_arg(0) == 'resources') {
+        if (func_num_args() > 1 && func_get_arg(0) == 'resources') {
             return $this->getResources(func_get_arg(1));
         }
         $filename = implode('/', func_get_args());
@@ -120,7 +142,7 @@ class Explorer
         $r = new stdClass();
         $r->apiVersion = (string)$this->restler->getRequestedApiVersion();
         $r->swaggerVersion = static::SWAGGER_VERSION;
-        $r->apis = $this->apis();
+        $r->apis = $this->apis($r->apiVersion);
         $r->authorizations = $this->authorizations();
         $r->info = static::$info;
         return $r;
@@ -136,8 +158,8 @@ class Explorer
         $r->basePath = $this->restler->getBaseUrl();
         $r->resourcePath = "/$id";
 
-        $r->apis = $this->apis($id);
-        $r->models = new stdClass();
+        $r->apis = $this->apis($r->apiVersion, $id);
+        $r->models = (object)$this->models;
 
         $r->produces = $this->restler->getProducedMimeTypes();
         $r->consumes = $this->restler->getConsumedMimeTypes();
@@ -145,29 +167,225 @@ class Explorer
         return $r;
     }
 
-    private function apis($path = false)
+    private function apis($version = 1, $resource = false)
     {
-        if (!static::$accessControlFunction && Defaults::$accessControlFunction)
-            static::$accessControlFunction = Defaults::$accessControlFunction;
-        $version = $this->restler->getRequestedApiVersion();
-        $allRoutes = Util::nestedValue(Routes::toArray(), "v$version");
-        $map = array();
+        $map = Routes::findAll(static::$excludedPaths + array($this->base), static::$excludedHttpMethods, $version);
         $r = array();
-        if (isset($allRoutes['*'])) {
-            $this->filteredApiListing($allRoutes['*'], $map, $version);
-            unset($allRoutes['*']);
-        }
-        $this->filteredApiListing($allRoutes, $map, $version);
-        foreach ($map as $path => $description) {
-            if (!String::contains($path, '{')) {
-                //add id
+        $a = array();
+        foreach ($map as $path => $data) {
+            $route = $data[0]['route'];
+            $access = $data[0]['access'];
+            if ($access && !String::contains($path, '{')) {
                 $r[] = array(
-                    'path' => $path . $this->formatString,
-                    'description' => $description
+                    'path' => "/$path", //"/$this->base/resources/$path",
+                    'description' => ''
+                    // Util::nestedValue($route, 'metadata', 'classDescription') ? : ''
                 );
+            }
+            if (static::$hideProtected && !$access)
+                continue;
+            foreach ($data as $item) {
+                $route = $item['route'];
+                $access = $item['access'];
+                $url = $route['url']; //end(explode($path . '/', $route['url'], 2));
+                $a[$path][] = array(
+                    'path' => "/$url", //str_replace($path, '', $route['url']),
+                    'description' =>
+                        Util::nestedValue($route, 'metadata', 'classDescription') ? : '',
+                    'operations' => array($this->operation($route))
+                );
+            }
+            //var_dump($route['metadata']['throws']);
+            //die();
+        }
+        //echo json_encode(($a), JSON_PRETTY_PRINT) . PHP_EOL;
+        if ($resource && isset($a[$resource]))
+            return $a[$resource];
+        return $r;
+    }
+
+    private function operation($route)
+    {
+        $r = new stdClass();
+        $r->method = $route['httpMethod'];
+        $r->nickname = $this->nickname($route);
+        $r->parameters = $this->parameters($route);
+
+        $m = $route['metadata'];
+
+        $r->summary = isset($m['description'])
+            ? $m['description']
+            : '';
+        $r->notes = isset($m['longDescription'])
+            ? $m['longDescription']
+            : '';
+        $r->responseMessages = $this->responseMessages($route);
+        $r->type = Util::nestedValue($m, 'return', 'type');
+
+        if (is_null($r->type)) {
+            $r->type = 'array';
+        } elseif ($r->type == 'null') {
+            $r->type = 'void';
+        } elseif (String::contains($r->type, '|')) {
+            $r->type = 'array';
+        }
+
+        //TODO: add $r->authorizations
+        //A list of authorizations required to execute this operation. While not mandatory, if used, it overrides
+        //the value given at the API Declaration's authorizations. In order to completely remove API Declaration's
+        //authorizations completely, an empty object ({}) may be applied.
+        //TODO: add $r->produces
+        //TODO: add $r->consumes
+        //A list of MIME types this operation can produce/consume. This is overrides the global produces definition at the root of the API Declaration. Each string value SHOULD represent a MIME type.
+        //TODO: add $r->deprecated
+        //Declares this operation to be deprecated. Usage of the declared operation should be refrained. Valid value MUST be either "true" or "false". Note: This field will change to type boolean in the future.
+        return $r;
+    }
+
+    private function parameters(array $route)
+    {
+        $r = array();
+        foreach ($route['metadata']['param'] as $param) {
+            $r[] = $this->parameter(
+                new ValidationInfo($param),
+                isset($param['description']) ? $param['description'] : ''
+            );
+        }
+        return $r;
+    }
+
+    private function parameter(ValidationInfo $info, $description = '')
+    {
+        $p = new stdClass();
+        $p->name = $info->name;
+        $this->setType($p, $info);
+        if (empty($info->children) || $info->type != 'array') {
+            //primitives
+            if ($info->default)
+                $p->defaultValue = $info->default;
+            if ($info->choice)
+                $p->enum = $info->choice;
+            if ($info->min)
+                $p->minimum = $info->min;
+            if ($info->max)
+                $p->maximum = $info->max;
+            //TODO: $p->items and $p->uniqueItems boolean
+        }
+        $p->description = $description;
+        $p->paramType = $info->from; //$info->from == 'body' ? 'form' : $info->from;
+        $p->required = $info->required;
+        $p->allowMultiple = false;
+        return $p;
+    }
+
+    private function responseMessages(array $route)
+    {
+        $r = array();
+        if (is_array($throws = Util::nestedValue($route, 'metadata', 'throws'))) {
+            foreach ($throws as $message) {
+                $m = (object)$message;
+                //TODO: add $m->responseModel from composer class
+                $r[] = $m;
             }
         }
         return $r;
+    }
+
+    private function model($type, array $children)
+    {
+        $type = Util::getShortName($type);
+        if (isset($this->models[$type]))
+            return $this->models[$type];
+        $r = new stdClass();
+        $r->id = $type;
+        $r->description = "$type Model"; //TODO: enhance this on Router
+        $r->required = array();
+        $r->properties = array();
+        foreach ($children as $child) {
+            $info = new ValidationInfo($child);
+            $p = new stdClass();
+            $this->setType($p, $info);
+            $p->description = $child['description'];
+            if ($info->default)
+                $p->defaultValue = $info->default;
+            if ($info->choice)
+                $p->enum = $info->choice;
+            if ($info->min)
+                $p->minimum = $info->min;
+            if ($info->max)
+                $p->maximum = $info->max;
+            if ($info->required)
+                $r->required[] = $info->name;
+            $r->properties[$info->name] = $p;
+        }
+        //TODO: add $r->subTypes https://github.com/wordnik/swagger-spec/blob/master/versions/1.2.md#527-model-object
+        //TODO: add $r->discriminator https://github.com/wordnik/swagger-spec/blob/master/versions/1.2.md#527-model-object
+        $this->models[$type] = $r;
+        return $r;
+    }
+
+    private function setType(&$object, ValidationInfo $info)
+    {
+        //TODO: proper type management
+        if ($info->type == 'array') {
+            if ($info->children) {
+                $this->model($info->contentType, $info->children);
+                $object->items = (object)array(
+                    '$ref' => $info->contentType
+                );
+            } elseif ($info->contentType) {
+                $object->items = (object)array(
+                    'type' => $info->contentType
+                );
+            } else {
+                $object->items = (object)array(
+                    'type' => 'string'
+                );
+            }
+        } elseif ($info->children) {
+            $this->model($info->type, $info->children);
+        } elseif ($t = Util::nestedValue(static::$dataTypeAlias, strtolower($info->type))) {
+            if (is_array($t)) {
+                $info->type = $t[0];
+                $object->format = $t[1];
+            } else {
+                $info->type = $t;
+            }
+        } else {
+            $info->type = 'string';
+        }
+        $object->type = $info->type;
+        $has64bit = PHP_INT_MAX > 2147483647;
+        if ($object->type == 'integer') {
+            $object->format = $has64bit
+                ? 'int64'
+                : 'int32';
+        } elseif ($object->type == 'number') {
+            $object->format = $has64bit
+                ? 'double'
+                : 'float';
+        }
+    }
+
+    private function nickname(array $route)
+    {
+        static $hash = array();
+        $method = $route['methodName'];
+        if (isset(static::$prefixes[$method])) {
+            $method = static::$prefixes[$method];
+        } else {
+            $method = str_replace(
+                array_keys(static::$prefixes),
+                array_values(static::$prefixes),
+                $method
+            );
+        }
+        while (isset($hash[$method]) && $route['url'] != $hash[$method]) {
+            //create another one
+            $method .= '_';
+        }
+        $hash[$method] = $route['url'];
+        return $method;
     }
 
     private function authorizations()
@@ -179,68 +397,5 @@ class Explorer
             'keyname' => 'api_key',
         );
         return $r;
-    }
-
-    private function filteredApiListing(array $allRoutes, array &$map, $version = 1)
-    {
-        foreach ($allRoutes as $fullPath => $routes) {
-            $path = explode('/', $fullPath);
-            $resource = isset($path[0]) ? $path[0] : '';
-            if (String::beginsWith($resource, $this->base) || String::endsWith($resource, 'index'))
-                continue;
-            foreach ($routes as $httpMethod => $route) {
-                if (in_array($httpMethod, static::$excludedHttpMethods)) {
-                    continue;
-                }
-                if (!$this->verifyAccess($route)) {
-                    continue;
-                }
-
-                foreach (static::$excludedPaths as $exclude) {
-                    if (empty($exclude)) {
-                        if ($fullPath == $exclude)
-                            continue 2;
-                    } elseif (String::beginsWith($fullPath, $exclude)) {
-                        continue 2;
-                    }
-                }
-
-                $res = $resource
-                    ? ($version == 1 ? "/$this->base/resources/$resource" : "/v$version/resources/$resource-v$version")
-                    : ($version == 1 ? "/$this->base/resources/root" : "/v$version/resources/root-v$version");
-
-                if (empty($map[$res])) {
-                    $map[$res] = isset(
-                    $route['metadata']['classDescription'])
-                        ? $route['metadata']['classDescription'] : '';
-                }
-            }
-        }
-    }
-
-    /**
-     * Verifies that the requesting user is allowed to view the docs for this API
-     *
-     * @param $route
-     *
-     * @return boolean True if the user should be able to view this API's docs
-     */
-    protected function verifyAccess($route)
-    {
-        if (
-            static::$hideProtected
-            && !$this->_authenticated
-            && $route['accessLevel'] > 1
-        ) {
-            return false;
-        }
-        if ($this->_authenticated
-            && static::$accessControlFunction
-            && (!call_user_func(
-                static::$accessControlFunction, $route['metadata']))
-        ) {
-            return false;
-        }
-        return true;
     }
 } 
