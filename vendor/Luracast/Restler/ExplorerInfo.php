cbb54aba97e5a0406cd20d64ae0e025f405983af
Luracast <arul@luracast.com>
2014-06-13 16:03:50 +0800

    Keeping Explorer metadata in ExplorerInfo class as static properties for easy customisation

---
+++ vendor/Luracast/Restler/ExplorerInfo.php
@@ -0,0 +1,12 @@
+<?php namespace Luracast\Restler;
+
+
+class ExplorerInfo
+{
+    public static $title = 'Restler API Explorer';
+    public static $description = 'Live API Documentation';
+    public static $termsOfServiceUrl = null;
+    public static $contact = 'arul@luracast.com';
+    public static $license = 'LGPL-2.1';
+    public static $licenseUrl = 'https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html';
+}
