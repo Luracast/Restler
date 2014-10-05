5494b38f0ec8dcc1a18ec5d3aa196a603176c68b
Luracast <arul@luracast.com>
2014-10-05 15:18:27 +0800

    Fixes Routes to map parameters correctly to query string when using @url comments. Closes #359

--- vendor/Luracast/Restler/Routes.php
+++ vendor/Luracast/Restler/Routes.php
@@ -237,13 +237,13 @@ class Routes
                         }
                     }
                     $url = preg_replace_callback('/{[^}]+}|:[^\/]+/',
-                        function ($matches) use ($call) {
+                        function ($matches) use ($copy) {
                             $match = trim($matches[0], '{}:');
-                            $index = $call['arguments'][$match];
+                            $index = $copy['arguments'][$match];
                             return '{' .
                             Routes::typeChar(isset(
-                            $call['metadata']['param'][$index]['type'])
-                                ? $call['metadata']['param'][$index]['type']
+                                $copy['metadata']['param'][$index]['type'])
+                                ? $copy['metadata']['param'][$index]['type']
                                 : null)
                             . $index . '}';
                         }, $url);
