36f36b9759e3cfb2a8f9561a83369f3b2ca7efda
Luracast <arul@luracast.com>
2014-11-10 16:57:48 +0800

    Adding Redirect::back() to go back to previous page

--- vendor/Luracast/Restler/Redirect.php
+++ vendor/Luracast/Restler/Redirect.php
@@ -32,11 +32,13 @@ class Redirect
         /** @var $r Restler */
         $r = Scope::get('Restler');
         $base = $r->getBaseUrl() . '/';
-        if (0 !== strpos($url, 'http'))
+        if (0 !== strpos($url, 'http')) {
             $url = $base . $url;
+        }
         if (!empty($flashData) || $base . $r->url !== $url || Util::getRequestMethod() != 'GET') {
-            if ($r->responseFormat instanceof JsonFormat)
+            if ($r->responseFormat instanceof JsonFormat) {
                 return array('redirect' => $url);
+            }
             if (!empty($params)) {
                 $url .= '?' . http_build_query($params);
             }
@@ -48,6 +50,19 @@ class Redirect
             header("Location: $url");
             die('');
         }
+
         return array();
+    }
+
+    /**
+     * Redirect back to the previous page
+     *
+     * Makes use of http referrer for redirection
+     *
+     * @return array
+     */
+    public static function back()
+    {
+        return static::to($_SERVER['HTTP_REFERER']);
     }
 }
