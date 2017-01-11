581d8d6dc5d37f439765f89725a92f85e98f1826
Luracast <arul@luracast.com>
2017-01-11 11:42:36 +0800

    Fix: PHP 7 compatibility by renaming String class as Text

--- vendor/Luracast/Restler/Data/Text.php
+++ vendor/Luracast/Restler/Data/Text.php
@@ -11,7 +11,7 @@ namespace Luracast\Restler\Data;
  * @license    http://www.opensource.org/licenses/lgpl-license.php LGPL
  * @link       http://luracast.com/products/restler/
  */
-class String
+class Text
 {
     /**
      * Given haystack contains the needle or not?
