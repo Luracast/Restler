48876ad2bf9e4497bf170c85aeff6d9a39e01ed3
Luracast <arul@luracast.com>
2018-01-04 13:08:02 +0800

    Improve compatibility. Closes #530

--- vendor/restler.php
+++ vendor/restler.php
@@ -1,7 +1,7 @@
 <?php
 if (is_readable(__DIR__ . '/autoload.php')) {
     //if composer auto loader is found use it
-    $loader = require_once __DIR__ . '/autoload.php';
+    $loader = require __DIR__ . '/autoload.php';
     $loader->setUseIncludePath(true);
     class_alias('Luracast\\Restler\\Restler', 'Restler');
 } else {
