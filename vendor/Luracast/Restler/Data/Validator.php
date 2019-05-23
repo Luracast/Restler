aaa341e02be2305109abbd08c7e1b2a9f89cb0d9
Arul Kumaran <arul@logicalsteps.net>
2019-05-23 11:29:24 +0800

    Fixes associative arrays fixing logic to keep  only their non numeric indices

--- vendor/Luracast/Restler/Data/Validator.php
+++ vendor/Luracast/Restler/Data/Validator.php
@@ -615,7 +615,7 @@ class Validator implements iValidate
                             if ($contentType == 'indexed') {
                                 $input = $info->filterArray($input, true);
                             } elseif ($contentType == 'associative') {
-                                $input = $info->filterArray($input, true);
+                                $input = $info->filterArray($input, false);
                             }
                         } elseif (
                             $contentType == 'indexed' &&
