9da8bfb90ef0eb9f23be240dab59c0c6c26d3223
Arul <arul@luracast.com>
2020-06-29 08:15:35 +0800

    Removes unused method

--- public/examples/_014_oauth2_client/Auth/Curl.php
+++ public/examples/_014_oauth2_client/Auth/Curl.php
@@ -19,14 +19,16 @@ class Curl
     }
 
     /**
-    * Send a request to the server, receive a response
-    *
-    * @param  string   $apiPath       Request API path
-    * @param  array    $parameters    Parameters
-    * @param  string   $httpMethod    HTTP method to use
-    *
-    * @return string   HTTP response
-    */
+     * Send a request to the server, receive a response
+     *
+     * @param string $url        Request API path
+     * @param array  $parameters Parameters
+     * @param string $httpMethod HTTP method to use
+     *
+     * @param array  $options
+     *
+     * @return string   HTTP response
+     */
     public function request($url, array $parameters = array(), $httpMethod = 'GET', array $options = array())
     {
         $options = array_merge($this->options, $options);
@@ -36,34 +38,30 @@ class Curl
 
         if ('POST' === $httpMethod) {
             $curlOptions += array(
-                CURLOPT_POST  => true,
+                CURLOPT_POST => true,
             );
-        }
-        elseif ('PUT' === $httpMethod) {
+        } elseif ('PUT' === $httpMethod) {
             $curlOptions += array(
-                CURLOPT_POST  => true, // This is so cURL doesn't strip CURLOPT_POSTFIELDS
+                CURLOPT_POST          => true, // This is so cURL doesn't strip CURLOPT_POSTFIELDS
                 CURLOPT_CUSTOMREQUEST => 'PUT',
             );
-        }
-        elseif ('DELETE' === $httpMethod) {
+        } elseif ('DELETE' === $httpMethod) {
             $curlOptions += array(
                 CURLOPT_CUSTOMREQUEST => 'DELETE',
             );
         }
 
-        if (!empty($parameters))
-        {
-            if('GET' === $httpMethod)
-            {
+        if (!empty($parameters)) {
+            if ('GET' === $httpMethod) {
                 $queryString = utf8_encode($this->buildQuery($parameters));
                 $url .= '?' . $queryString;
             } elseif ('POST' === $httpMethod) {
                 $curlOptions += array(
-                    CURLOPT_POSTFIELDS  => $parameters,
+                    CURLOPT_POSTFIELDS => $parameters,
                 );
             } else {
                 $curlOptions += array(
-                    CURLOPT_POSTFIELDS  => json_encode($parameters)
+                    CURLOPT_POSTFIELDS => json_encode($parameters)
                 );
                 $headers[] = 'Content-Type: application/json';
             }
@@ -71,16 +69,16 @@ class Curl
             $headers[] = 'Content-Length: 0';
         }
 
-        $this->debug('send '.$httpMethod.' request: '.$url);
+        $this->debug('send ' . $httpMethod . ' request: ' . $url);
 
         $curlOptions += array(
-            CURLOPT_URL             => $url,
-            CURLOPT_PORT            => $options['http_port'],
-            CURLOPT_USERAGENT       => $options['user_agent'],
-            CURLOPT_RETURNTRANSFER  => true,
-            CURLOPT_TIMEOUT         => $options['timeout'],
-            CURLOPT_HTTPHEADER      => $headers,
-            CURLOPT_SSL_VERIFYPEER  => $options['verifyssl'],
+            CURLOPT_URL            => $url,
+            CURLOPT_PORT           => $options['http_port'],
+            CURLOPT_USERAGENT      => $options['user_agent'],
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_TIMEOUT        => $options['timeout'],
+            CURLOPT_HTTPHEADER     => $headers,
+            CURLOPT_SSL_VERIFYPEER => $options['verifyssl'],
         );
 
         if (ini_get('open_basedir') == '' && ini_get('safe_mode') != 'On') {
@@ -98,21 +96,6 @@ class Curl
         $response = $this->doCurlCall($curlOptions);
 
         return $response;
-    }
-
-    /**
-     * Get a JSON response and transform it to a PHP array
-     *
-     * @return  array   the response
-     */
-    protected function decodeResponse($response)
-    {
-        // "false" means a failed curl request
-        if (false === $response['response']) {
-            $this->debug(print_r($response, true));
-            return false;
-        }
-        return parent::decodeResponse($response);
     }
 
     protected function doCurlCall(array $curlOptions)
@@ -138,9 +121,8 @@ class Curl
 
     protected function debug($message)
     {
-        if($this->options['debug'])
-        {
-            print $message."\n";
+        if ($this->options['debug']) {
+            print $message . "\n";
         }
     }
 }
