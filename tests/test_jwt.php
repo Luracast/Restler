95d8cea3986fb31366e7519ca978a95310f9eec9
Arul <arul@luracast.com>
2021-08-11 23:15:51 +0800

    Testing JsonWebToken

---
+++ tests/test_jwt.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+use Firebase\JWT\JWT;
+use GuzzleHttp\Psr7\ServerRequest;
+use Luracast\Restler\Auth\JsonWebToken;
+use Luracast\Restler\ResponseHeaders;
+use Luracast\Restler\UserIdentifier;
+
+include __DIR__ . "/../vendor/autoload.php";
+$privateKey = <<<EOD
+-----BEGIN RSA PRIVATE KEY-----
+MIICXAIBAAKBgQC8kGa1pSjbSYZVebtTRBLxBz5H4i2p/llLCrEeQhta5kaQu/Rn
+vuER4W8oDH3+3iuIYW4VQAzyqFpwuzjkDI+17t5t0tyazyZ8JXw+KgXTxldMPEL9
+5+qVhgXvwtihXC1c5oGbRlEDvDF6Sa53rcFVsYJ4ehde/zUxo6UvS7UrBQIDAQAB
+AoGAb/MXV46XxCFRxNuB8LyAtmLDgi/xRnTAlMHjSACddwkyKem8//8eZtw9fzxz
+bWZ/1/doQOuHBGYZU8aDzzj59FZ78dyzNFoF91hbvZKkg+6wGyd/LrGVEB+Xre0J
+Nil0GReM2AHDNZUYRv+HYJPIOrB0CRczLQsgFJ8K6aAD6F0CQQDzbpjYdx10qgK1
+cP59UHiHjPZYC0loEsk7s+hUmT3QHerAQJMZWC11Qrn2N+ybwwNblDKv+s5qgMQ5
+5tNoQ9IfAkEAxkyffU6ythpg/H0Ixe1I2rd0GbF05biIzO/i77Det3n4YsJVlDck
+ZkcvY3SK2iRIL4c9yY6hlIhs+K9wXTtGWwJBAO9Dskl48mO7woPR9uD22jDpNSwe
+k90OMepTjzSvlhjbfuPN1IdhqvSJTDychRwn1kIJ7LQZgQ8fVz9OCFZ/6qMCQGOb
+qaGwHmUK6xzpUbbacnYrIM6nLSkXgOAwv7XXCojvY614ILTK3iXiLBOxPu5Eu13k
+eUz9sHyD6vkgZzjtxXECQAkp4Xerf5TGfQXGXhxIX52yH+N2LtujCdkQZjXAsGdm
+B2zNzvrlgRmgBrklMTrMYgm1NPcW+bRLGcwgW2PTvNM=
+-----END RSA PRIVATE KEY-----
+EOD;
+
+$publicKey = <<<EOD
+-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC8kGa1pSjbSYZVebtTRBLxBz5H
+4i2p/llLCrEeQhta5kaQu/RnvuER4W8oDH3+3iuIYW4VQAzyqFpwuzjkDI+17t5t
+0tyazyZ8JXw+KgXTxldMPEL95+qVhgXvwtihXC1c5oGbRlEDvDF6Sa53rcFVsYJ4
+ehde/zUxo6UvS7UrBQIDAQAB
+-----END PUBLIC KEY-----
+EOD;
+
+$time = time();
+$payload = array(
+    "iss" => "example.org",
+    "aud" => "example.com",
+    "iat" => $time,
+    "nbf" => $time,
+    "sub" => "123",
+);
+
+$jwt = JWT::encode($payload, $privateKey, 'RS256');
+
+var_dump($jwt);
+$request = new ServerRequest('GET', '/', ['Authorization' => "Bearer $jwt"]);
+$user = new UserIdentifier($request);
+JsonWebToken::$publicKey = $publicKey;
+$auth = new JsonWebToken();
+try {
+    var_dump($auth->_isAllowed($request, $user, new ResponseHeaders()));
+} catch (Exception $e) {
+    var_dump($e->getMessage());
+}
