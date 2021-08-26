e3714f98b1018b6c5f1e42b125bc0df81decc66d
Arul <arul@luracast.com>
2021-08-26 13:46:50 +0800

    Fixes cors test

--- tests/test_cors.php
+++ tests/test_cors.php
@@ -4,6 +4,7 @@ use Luracast\Restler\Defaults;
 use Luracast\Restler\Restler;
 use Luracast\Restler\Routes;
 use Luracast\Restler\Utils\Dump;
+use Psr\Http\Message\ResponseInterface;
 use RingCentral\Psr7\ServerRequest;
 
 include __DIR__ . "/../vendor/autoload.php";
@@ -17,24 +18,31 @@ class Home
 }
 
 Defaults::$crossOriginResourceSharing = true;
-Routes::addAPI(Home::class, '');
+Routes::mapApiClasses(['' => Home::class]);
 
 $h = new Restler();
 
-$request = new ServerRequest('OPTIONS', 'http://localhost:4000', [
-    'Access-Control-Request-Method' => 'GET',
-    'Origin' => 'http://localhost:3000',
-    'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
-    'Access-Control-Request-Headers' => 'authorization,content-type',
-    'Accept' => '*/*',
-    'DNT' => 1,
-    'Referer' => 'http://localhost:3000/login',
-    'Accept-Encoding' => 'gzip, deflate, br',
-    'Accept-Language' => 'en-US,en;q=0.9,ta;q=0.8'
-], '');
+$request = new ServerRequest(
+    'OPTIONS', 'http://localhost:4000',
+    [
+        'Access-Control-Request-Method' => 'GET',
+        'Origin' => 'http://localhost:3000',
+        'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+        'Access-Control-Request-Headers' => 'authorization,content-type',
+        'Accept' => '*/*',
+        'DNT' => 1,
+        'Referer' => 'http://localhost:3000/login',
+        'Accept-Encoding' => 'gzip, deflate, br',
+        'Accept-Language' => 'en-US,en;q=0.9,ta;q=0.8'
+    ],
+    ''
+);
 
-$response = $h->handle($request);
+echo Dump::request($request);
 
-echo Dump::response($response, true);
+$h->handle($request)->then(function (ResponseInterface $response) {
+    echo Dump::response($response, true);
+});
+
 
 
