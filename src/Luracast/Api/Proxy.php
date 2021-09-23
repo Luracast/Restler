d742baf7012cd1e74682698ef0c820e12540a60b
Arul <arul@luracast.com>
2021-09-23 19:00:55 +0800

    Proxy class for proxying some requests to other servers

--- src/Luracast/Api/Proxy.php
+++ src/Luracast/Api/Proxy.php
@@ -4,19 +4,15 @@ namespace Luracast\Restler\Api;
 
 
 use GuzzleHttp\Client;
-use GuzzleHttp\DefaultHandler;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Psr7\Uri;
 use Luracast\Restler\Exceptions\HttpException;
-use Luracast\Restler\Utils\Dump;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
-//use Yurun\Util\Swoole\Guzzle\SwooleHandler;
-
 class Proxy
 {
-    public static $mappings = [
+    public static array $mappings = [
         'google' => 'https://www.google.com/search'
     ];
     private ?ServerRequestInterface $request;
@@ -24,7 +20,6 @@ class Proxy
     public function __construct(ServerRequestInterface $request)
     {
         $this->request = $request;
-        //DefaultHandler::setDefaultHandler(SwooleHandler::class);
     }
 
     /**
@@ -49,16 +44,6 @@ class Proxy
             ->withHost($uri->getHost())
             ->withPort($uri->getPort());
         $request = $this->request->withUri($newUri);
-        //die('<pre>' . Dump::request($request));
-        /*
-        $handler = new CurlHandler(); //new CurlMultiHandler();
-        $stack = HandlerStack::create($handler);
-        $client = new Client(['handler' => $stack]);
-        */
-        //go(function () use ($request) {
-        $client = new Client();
-        echo(Dump::response($client->send($request)));
-        return $client->send($request);
-        //});
+        return (new Client)->send($request)->withoutHeader('transfer-encoding');
     }
 }
