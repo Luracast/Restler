33ca026c41ed0e0df836c11c3b841c961e742e84
Arul <arul@luracast.com>
2022-12-13 14:37:58 +0800

    Updates the dependencies and changes Spreadsheet class to use avadim\FastExcelWriter\Excel instead of Box\Spout

--- src/Luracast/MediaTypes/Spreadsheet.php
+++ src/Luracast/MediaTypes/Spreadsheet.php
@@ -4,9 +4,7 @@
 namespace Luracast\Restler\MediaTypes;
 
 
-use Box\Spout\Common\Entity\Style\Color;
-use Box\Spout\Writer\Common\Creator\Style\StyleBuilder;
-use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use avadim\FastExcelWriter\Excel;
 use Luracast\Restler\Contracts\DownloadableFileMediaTypeInterface;
 use Luracast\Restler\Defaults;
 use Luracast\Restler\Exceptions\HttpException;
@@ -19,20 +17,31 @@ class Spreadsheet extends Dependent implements DownloadableFileMediaTypeInterfac
     public const MIME = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
     public const EXTENSION = 'xlsx';
 
+    public bool $firstRowHasFormats = false;
+    public string $title = 'Sheet 1';
+    public string $filename = 'Spreadsheet';
+
+    public static array $headerStyle = [
+        'font' => 'bold',
+        'text-align' => 'center',
+        'vertical-align' => 'center',
+        'border' => 'thin',
+        'fill' => '#E6E6E6',
+        'format' => '@'
+    ];
+
     public function __construct(Convert $convert)
     {
         parent::__construct($convert);
     }
 
     /**
-     * @return array {@type associative}
+     * @return array {@format associative}
      *               CLASS_NAME => vendor/project:version
      */
     public static function dependencies(): array
     {
-        return [
-            'Box\Spout\Common\Entity\Row' => 'box/spout:dev-master'
-        ];
+        return ['avadim\FastExcelWriter\Excel' => 'avadim/fast-excel-writer:^2.3'];
     }
 
     /**
@@ -42,37 +51,41 @@ class Spreadsheet extends Dependent implements DownloadableFileMediaTypeInterfac
      * @param bool $humanReadable
      * @return false|string
      * @throws HttpException
-     * @throws \Box\Spout\Common\Exception\IOException
-     * @throws \Box\Spout\Common\Exception\UnsupportedTypeException
-     * @throws \Box\Spout\Writer\Exception\WriterNotOpenedException
      */
-    public function encode($data, ResponseHeaders $responseHeaders, bool $humanReadable = false)
+    public function encode($data, ResponseHeaders $responseHeaders, bool $humanReadable = false): string
     {
         $data = $this->convert->toArray($data);
         if (!is_writable(Defaults::$cacheDirectory)) {
-            throw new HttpException(500, 'Spreadsheet needs Defaults::$cacheDirectory to be writable.');
+            throw new HttpException(
+                500,
+                'Spreadsheet needs Defaults::$cacheDirectory to be writable.'
+            );
         }
         if (is_array($data) && array_values($data) == $data) {
-            //if indexed array
             $file = Defaults::$cacheDirectory . '/spreadsheet' . microtime() . '.' . $this->extension();
-            $writer = WriterEntityFactory::createWriter($this->extension());
-            $writer->openToFile($file);
+            $excel = Excel::create([$this->title]);
+            $sheet = $excel->getSheet();
             $row = array_shift($data);
-            $style = (new StyleBuilder())->setFontBold()->setBackgroundColor(Color::rgb(230, 230, 230))->build();
-            if (array_values($row) == $row) {
+            if ($this->firstRowHasFormats) {
+                $sheet->writeHeader($row, static::$headerStyle);
+            } elseif (array_values($row) == $row) {
                 //write header with first row values
-                $writer->addRow(WriterEntityFactory::createRowFromArray(array_values($row), $style));
+                $sheet->writeHeader($row, static::$headerStyle);
             } else {
                 //write header with keys
-                $writer->addRow(WriterEntityFactory::createRowFromArray(array_keys($row), $style));
-                $writer->addRow(WriterEntityFactory::createRowFromArray(array_values($row)));
+                $sheet->writeHeader(array_keys($row), static::$headerStyle);
+                $sheet->writeRow(array_values($row));
             }
             foreach ($data as $row) {
-                $writer->addRow(WriterEntityFactory::createRowFromArray(array_values($row)));
+                $sheet->writeRow(array_values($row));
             }
-            $writer->close();
+            $excel->save($file);
             $export = file_get_contents($file);
             unlink($file);
+            if (!isset($responseHeaders['Content-Disposition'])) {
+                $responseHeaders['Content-Disposition'] = 'attachment; filename='
+                    . $this->filename . '.' . static::EXTENSION;
+            }
             return $export;
         }
         throw new HttpException(500, 'Unsupported data for ' . strtoupper($this->extension()) . ' MediaType');
