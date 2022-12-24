bb2e740f92ae46e117bc24e42fa868e2f208895b
Arul <arul@luracast.com>
2022-12-24 20:22:48 +0800

    Adds convenient methods to add headers

--- src/Luracast/ResponseHeaders.php
+++ src/Luracast/ResponseHeaders.php
@@ -7,7 +7,47 @@ use ArrayAccess;
 
 class ResponseHeaders implements ArrayAccess
 {
-    private $container = [];
+    private array $container = [];
+    private ?int $status = null;
+    private int $start = 0;
+    private int $end = 0;
+    private int $length = 0;
+
+    public function setStatus($code): void
+    {
+        $this->status = $code;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setRange(int $start, int $end, ?int $total = null): void
+    {
+        $this->start = $start;
+        $this->end = $end;
+        if (!$total) {
+            $total = $this->getContentLength() ?? $end;
+        }
+        $this->setContentLength($end - $start);
+        $this->container['Content-Range'] = "bytes $start-$end/$total";
+    }
+
+    public function getRange(): array
+    {
+        return [$this->start, $this->end, $this->length];
+    }
+
+    public function getContentLength(): int
+    {
+        return $this->length ?? $this->container['Content-Length'] ?? 0;
+    }
+
+    public function setContentLength(int $length): void
+    {
+        $this->container['Content-Length'] = $this->length = $length;
+    }
 
     public function getArrayCopy(): array
     {
